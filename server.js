2b4b3c77df44848fc9d1fbd19ef13ed029fbf4fb
Akinrolabu Lolade <busayoololade@yahoo.com>
2019-09-14 17:38:59 +0100

    Added the Procfile file and place css,js,img folders into the static-files

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const path = require('path');
 const app = express();
 const PORT =1010;
 
-app.use(express.static('/css'));
-app.use(express.static('images'));
-app.use(express.static('/js'));
+app.use(express.static('static-files'));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname+'/index.html'));
