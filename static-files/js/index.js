30832464476ee2d811e391721a0ec5333c399559
Akinrolabu Lolade <busayoololade@yahoo.com>
2019-09-25 22:25:57 +0100

    Added more functionality

--- static-files/js/index.js
+++ static-files/js/index.js
@@ -3,6 +3,13 @@ console.log("Hello Lolade first server website");
 let button = document.querySelector('.go-up');
 
 let pageOffsetY= window.pageYOffset;
-window.scroll(function(){
-    console.log(window.pageYOffset);
-});
+
+window.onscroll=function(){
+    if(window.pageYOffset > 300){
+        button.classList.remove('add--item');
+        button.classList.add('show--item');
+    }else{
+        button.classList.remove('show--item');
+        button.classList.add('hide--item');
+    }
+};
