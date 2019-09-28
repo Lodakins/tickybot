644657308431121472c71f3ccd5a984745d41e6d
Akinrolabu Lolade <busayoololade@yahoo.com>
2019-09-28 04:08:15 +0100

    added new image and functionality to the javascript

--- static-files/js/index.js
+++ static-files/js/index.js
@@ -1,8 +1,6 @@
-console.log("Hello Lolade first server website");
-
 let button = document.querySelector('.go-up');
-
-let pageOffsetY= window.pageYOffset;
+const navbarLinks =document.querySelectorAll('.site-header__link-container a');
+const buttonlink= document.querySelector('.go-up a');
 
 window.onscroll=function(){
     if(window.pageYOffset > 300){
@@ -12,4 +10,35 @@ window.onscroll=function(){
         button.classList.remove('show--item');
         button.classList.add('hide--item');
     }
-};
+}; // function for the go-up button
+
+
+navbarLinks.forEach(element=>element.addEventListener('click',navbarLinkClick));
+
+
+function navbarLinkClick(event){
+    smoothScrolling(event);  // calls the smoothscrolling function
+}
+
+
+//smoth scrolling
+function smoothScrolling(event){
+    event.preventDefault();
+    const targetId= event.currentTarget.getAttribute('href');
+    window.scrollTo({
+        top: document.querySelector(targetId).offsetTop,
+        behavior: "smooth"
+    });
+}
+
+buttonlink.addEventListener('click',gotoTop);
+
+function gotoTop(event){
+    event.preventDefault();
+    const targetId= event.currentTarget.getAttribute('href');
+    console.log(targetId);
+    window.scrollTo({
+        top: document.querySelector(targetId).offsetTop,
+        behavior:'smooth'
+    });
+}
